cb971a833e99c5415db6734bb43dadb946e00428
argusyk <argusyk@gmail.com>
2025-11-16 14:10:27 +0200

    Create app.py for Chal8

---
+++ chal8/src/app.py
@@ -0,0 +1,53 @@
+from Crypto.Cipher import AES
+import os
+from Crypto.Util.Padding import pad, unpad
+from datetime import datetime, timedelta
+from flask import Flask, jsonify, send_from_directory
+import secrets
+
+# Random 16 Bytes key
+KEY = secrets.token_bytes(16) 
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+with open(current_dir+'/flag.txt', 'r') as f: FLAG = f.read().strip()
+
+app = Flask(__name__)
+
+@app.route('/')
+def serve_source():
+    return send_from_directory(current_dir, 'app.py')
+
+
+@app.route('/check_admin/<cookie>/<iv>/')
+def check_admin(cookie, iv):
+    cookie = bytes.fromhex(cookie)
+    iv = bytes.fromhex(iv)
+
+    try:
+        cipher = AES.new(KEY, AES.MODE_CBC, iv)
+        decrypted = cipher.decrypt(cookie)
+        unpadded = unpad(decrypted, 16)
+    except ValueError as e:
+        return {"error": str(e)}
+
+    if b"admin=True" in unpadded.split(b";"):
+        return {"flag": FLAG}
+    else:
+        return {"error": "Only admin can read the flag"}
+
+
+@app.route('/get_cookie/')
+def get_cookie():
+    expires_at = (datetime.today() + timedelta(days=1)).strftime("%s")
+    cookie = f"admin=False;expiry={expires_at}".encode()
+
+    iv = os.urandom(16)
+    padded = pad(cookie, 16)
+    cipher = AES.new(KEY, AES.MODE_CBC, iv)
+    encrypted = cipher.encrypt(padded)
+    ciphertext = iv.hex() + encrypted.hex()
+
+    return {"cookie": ciphertext}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
